30d1cee7f30a7cc6c3a8813d1458f8fb33c08575
sheth chirag <102785098+shethchirag@users.noreply.github.com>
2024-04-13 14:47:18 +0530

    nav image fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <div className="my-4 gap-2 h-[60px] rounded-lg bg-white flex items-center justify-center text-xl font-medium">
-      <img src="/public/firebase.svg" alt="firebase" />
+      <img src="/firebase.svg" alt="firebase" />
       <h1>Firebase Contact App</h1>
     </div>
   );
